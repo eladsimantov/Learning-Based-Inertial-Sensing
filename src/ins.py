bf1f2f60671e4cc39abdc13435154c50ac311613
Elad Siman Tov <62391381+eladsimantov@users.noreply.github.com>
2024-12-09 20:16:16 +0000

    feat: temporary improvements

--- src/ins.py
+++ src/ins.py
@@ -3,25 +3,40 @@
 import pandas as pd
 import numpy as np
 
-class INS:
-    def __init__(self, raw_data: pd.DataFrame, device_data: pd.DataFrame, time_data: pd.DataFrame):
-        self._raw_data = raw_data
-        self._device_data = device_data
-        self._time_data = time_data
+class Accelerometer:
+    def __init__(self):
         return 
     
-    def get_raw_data(self): 
-        return self._raw_data
-
-    def get_device_data(self): 
-        return self._device_data
-
-    def get_time_data(self): 
-        return self._time_data   
-
-    def calibrate_accelerometer(self):
-        self.get_raw_data()
+    def set_calibration_data(self, fx_up, fx_down, fy_up, fy_down, fz_up, fz_down):
+        """
+        Enter data as a dictionary of dataframes called: 'raw_data', 'device_data', 'time_data'
+        """
+        self._fx_up = fx_up
+        self._fx_down = fx_down
+        self._fy_up = fy_up
+        self._fy_down = fy_down
+        self._fz_up = fz_up
+        self._fz_down = fz_down
         return
+
+    def get_calibration_data(self):
+        return {
+            'fx_up': self._fx_up,
+            'fx_down': self._fx_down,
+            'fy_up': self._fy_up,
+            'fy_down': self._fy_down,
+            'fz_up': self._fz_up,
+            'fz_down': self._fz_down
+        }
+
+    @staticmethod
+    def calc_bias(mean_f_up, mean_f_down):
+        return (mean_f_up + mean_f_down)/2
     
+    @staticmethod
+    def calc_scale_factor(mean_f_up, mean_f_down, gravity):
+        return  (mean_f_up - mean_f_down - 2 * gravity)/(2 * gravity)
+
+        
 
     
