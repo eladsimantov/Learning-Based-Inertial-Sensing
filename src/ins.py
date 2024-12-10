41dc760e44dd87bc5d49ddb8a891345cf1692a1c
Elad Siman Tov <62391381+eladsimantov@users.noreply.github.com>
2024-12-10 10:14:37 +0000

    feat: add staticmethods for 6axis calibration

--- src/ins.py
+++ src/ins.py
@@ -1,33 +1,42 @@
-# src/INS/INS.py
-
+# src/ins.py
 import pandas as pd
 import numpy as np
 
 class Accelerometer:
-    def __init__(self):
+    def __init__(self, name: str):
+        self.__name = name
         return 
     
-    def set_calibration_data(self, fx_up, fx_down, fy_up, fy_down, fz_up, fz_down):
-        """
-        Enter data as a dictionary of dataframes called: 'raw_data', 'device_data', 'time_data'
-        """
+    def set_calibration_data(self, 
+                             fx_up: np.array, fx_down: np.array, 
+                             fy_up: np.array, fy_down: np.array, 
+                             fz_up: np.array, fz_down: np.array, 
+                             gravity: float):
         self._fx_up = fx_up
         self._fx_down = fx_down
         self._fy_up = fy_up
         self._fy_down = fy_down
         self._fz_up = fz_up
         self._fz_down = fz_down
+        self._gravity = gravity
         return
 
-    def get_calibration_data(self):
+    def get_calibration_data(self) -> dict:
         return {
             'fx_up': self._fx_up,
             'fx_down': self._fx_down,
             'fy_up': self._fy_up,
             'fy_down': self._fy_down,
             'fz_up': self._fz_up,
-            'fz_down': self._fz_down
+            'fz_down': self._fz_down,
+            'g': self._gravity
         }
+    
+    def get_M_errors_matrix(self) -> np.array:
+        f_dict = self.get_calibration_data()
+        f = np.array([f_dict["fx_down"], f_dict["fx_up"], f_dict["fy_down"], f_dict["fy_up"], f_dict["fx_down"], f_dict["fz_up"]])
+        M = self.calc_M_matrix(f, f_dict['g'])
+        return M 
 
     @staticmethod
     def calc_bias(mean_f_up, mean_f_down):
@@ -36,3 +45,11 @@ class Accelerometer:
     @staticmethod
     def calc_scale_factor(mean_f_up, mean_f_down, gravity):
         return  (mean_f_up - mean_f_down - 2 * gravity)/(2 * gravity)
+
+    @staticmethod
+    def calc_M_matrix(f: np.array, g: float) -> np.array:
+        G_mat = np.array([[-g, g, 0, 0, 0, 0], [0, 0, -g, g, 0, 0], [0, 0, 0, 0, -g, g]])
+        z = f - G_mat
+        A = np.array([[-g, g, 0, 0, 0, 0], [0, 0, -g, g, 0, 0], [0, 0, 0, 0, -g, g], [1,1,1,1,1,1]])
+        M = z @ A.T @ np.linalg.inv(A @ A.T)
+        return M
