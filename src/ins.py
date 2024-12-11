8127eb676e410b99c46e755b3c90d5e38c9bca25
Elad Siman Tov <62391381+eladsimantov@users.noreply.github.com>
2024-12-11 14:21:32 +0000

    feat: formalized simple bias and sf for sensor class

--- src/ins.py
+++ src/ins.py
@@ -27,6 +27,18 @@ class Sensor:
     
     def get_calibration_data(self) -> dict:
         return self._calibration_data
+    
+    @staticmethod
+    def calc_bias(mean_positive: np.ndarray, mean_negative: np.ndarray) -> np.ndarray:
+        return (mean_positive + mean_negative) / 2
+    
+    @staticmethod
+    def calc_scale_factor(mean_positive: np.ndarray, mean_negative: np.ndarray, forcing) -> np.ndarray:
+        """ 
+        For Gyroscope: forcing = omega
+        For Accelerometer: forcing = g
+        """
+        return (mean_positive - mean_negative - 2 * forcing)/(2 * forcing)
         
             
 class Gyroscope(Sensor):
@@ -39,6 +51,13 @@ class Gyroscope(Sensor):
         w = np.hstack((w_dict["wx_left"], w_dict["wx_right"], w_dict["wy_left"], w_dict["wy_right"], w_dict["wz_left"], w_dict["wz_right"]))
         M = self.calc_M_matrix(w)
         return M
+    
+    def get_bias(self) -> np.ndarray:
+        w_dict = self.get_calibration_data()
+        w_left = np.array([w_dict["wx_left"], w_dict["wy_left"], w_dict["wz_left"]])
+        w_right = np.array([w_dict["wx_right"], w_dict["wy_right"], w_dict["wz_right"]])
+        bias = self.calc_bias(w_left, w_right)
+        return bias
     
     @staticmethod
     def calc_bias(mean_w_left, mean_w_right) -> np.ndarray:
